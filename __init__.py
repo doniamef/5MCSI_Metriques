d1f808bfdd68265d20ad9ca3740b3c0a394b4223
doniamef <64195846+doniamef@users.noreply.github.com>
2024-10-18 15:54:33 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,16 +4,21 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
+from flask import jsonify
+import json
+
                                                                                                                                        
 app = Flask(__name__)                                                                                                                  
                                                                                                                                        
 @app.route('/')
 def hello_world():
-    return render_template('hello.html') #commd
+    return render_template('hello.html')
 
 @app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
+def contact():
+    return render_template("contact.html")
+
+
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -25,9 +30,32 @@ def meteo():
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
+  
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
+  
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
+
+
+#@app.route('/extract-minutes/<date_string>')
+#def extract_minutes(date_string):
+#   date_object = datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
+#    minutes = date_object.minute
+#    return jsonify({'minutes': minutes})
+
+@app.route('/commits/')
+def show_commits():
+    return render_template('commits.html')
+
+
+
+
+@app.route("/commits/")
+def moncommits():
+    return render_template("commits.html")
   
 if __name__ == "__main__":
   app.run(debug=True)
